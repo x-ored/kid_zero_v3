bf71b96bd8ffca8856e63b8f41b1df7589d50e60
Zak <ataxored@gmail.com>
2021-09-22 16:17:33 -0700

    Added splash Activity

--- app/src/main/java/com/eternal/kidzero/MainActivity.java
+++ app/src/main/java/com/eternal/kidzero/MainActivity.java
@@ -1,19 +1,7 @@
 package com.eternal.kidzero;
 
 import android.os.Bundle;
-
-import com.eternal.kidzero.ui.fragments.Auth.AuthFrag;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.fragment.NavHostFragment;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
