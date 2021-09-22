59653489e5dacd92b11c0216269e924d20af949a
Zak <ataxored@gmail.com>
2021-09-22 15:57:55 -0700

    Upload project

---
+++ app/src/main/java/com/eternal/kidzero/ui/fragments/BaseFrag.java
@@ -0,0 +1,16 @@
+package com.eternal.kidzero.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.eternal.kidzero.R;
+
+public class BaseFrag extends Fragment {
+
+
+}
---
+++ app/src/main/java/com/eternal/kidzero/ui/fragments/auth/AuthFrag.java
@@ -0,0 +1,28 @@
+package com.eternal.kidzero.ui.fragments.auth;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.eternal.kidzero.R;
+import com.eternal.kidzero.ui.fragments.BaseFrag;
+
+public class AuthFrag extends BaseFrag {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_auth, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
